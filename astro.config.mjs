946b6c1815f612d0d6ea0218a8e55dd6c9b694f4
zelink <739879236@qq.com>
2026-04-18 20:40:15 +0800

    配置 GitHub Pages 自动化部署

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,9 +36,9 @@ import { remarkImageGrid } from "./src/plugins/remark-image-grid.js";
 
 // https://astro.build/config
 export default defineConfig({
-	site: siteConfig.site_url,
+	site: "https://L-lty.github.io",
 
-	base: "/",
+	base: "/Leoblog",
 	trailingSlash: "always",
 
 	// 图像优化配置
